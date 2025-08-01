567fa3cff3e89668f3a0cad702461793ceef7a2c
WACHIRA.KE <ianwachira90@gmail.com>
2025-08-01 14:27:09 +0300

    Add files via upload

---
+++ assignment 1.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+
+sum_result = num1 + num2
+print(f"The sum of {num1} and {num2} is: {sum_result}")
+print(f"The result of your two numbers")
+print(f"Sum: {sum_result}")
+print("Thank you for using the addition program!")
